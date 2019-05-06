6fb1fbf6bd04ff880f215e76463cd2d0a8ae2f98
jjlebow@gmail.com <jlebow@uci.edu>
2019-05-06 15:52:24 -0700

    added a prefab for the exit portal

---
+++ Horror Maze/Assets/Standard Assets/Scripts-General/ExitPortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.GetComponent<Collider>().CompareTag("Player"))
+        {
+            Debug.Log("entered");
+            //Time.timeScale = 0;
+        }
+    }
+
+}
