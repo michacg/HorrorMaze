c79559d42b4c4521c20ecea89cf132aaa30350c1
jcvong <39819708+jcvong@users.noreply.github.com>
2019-06-02 16:06:39 -0700

    Flashes and fades wow

--- Horror Maze/Assets/Scripts/FadeManager.cs
+++ Horror Maze/Assets/Scripts/FadeManager.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class FadeManager : MonoBehaviour
 {
     public static FadeManager instance;
-    public Image screen;
+    public Image deathScreen;
+    public Image flashScreen;
     public float percentage = 0;
+    public bool inDeath = false;
+    private string[] storyLines;
 
     void Awake()
     {
@@ -22,6 +25,8 @@ public class FadeManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(instance);
+
+        //BuildStoryArray(Resources.Load<TextAsset>("Story.txt"));
     }
 
     public void StartDeath()
@@ -31,13 +36,46 @@ public class FadeManager : MonoBehaviour
 
     public IEnumerator FadeScreen()
     {
-        while (true)
+        inDeath = true;
+        bool hideWhite = false;
+
+        while (true) //white flash upon stepping on a trap
+        {
+            if (!hideWhite)
+            {
+                percentage += 0.125f;
+                float currentValue = Mathf.Lerp(0, 1, percentage);
+                flashScreen.color = new Color(flashScreen.color.r, flashScreen.color.g, flashScreen.color.b, currentValue);
+
+                if (percentage >= 1)
+                {
+                    percentage = 1;
+                    hideWhite = true;
+                }
+            }
+            else
+            {
+                percentage -= 0.125f;
+                float currentValue = Mathf.Lerp(0, 1, percentage);
+                flashScreen.color = new Color(flashScreen.color.r, flashScreen.color.g, flashScreen.color.b, currentValue);
+
+                if (percentage <= 0)
+                {
+                    percentage = 0;
+                    hideWhite = false;
+                    break;
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        while (true) //black fade after the white flash
         {
             percentage += 0.025f;
 
             float currentValue = Mathf.Lerp(0, 1, percentage);
 
-            screen.color = new Color(screen.color.r, screen.color.g, screen.color.b, currentValue);
+            deathScreen.color = new Color(deathScreen.color.r, deathScreen.color.g, deathScreen.color.b, currentValue);
 
             if (percentage >= 1)
             {
@@ -48,6 +86,13 @@ public class FadeManager : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
         yield return new WaitForSeconds(1.5f);
-        screen.color = new Color(screen.color.r, screen.color.g, screen.color.b, 0);
+        deathScreen.color = new Color(deathScreen.color.r, deathScreen.color.g, deathScreen.color.b, 0);
+        inDeath = false;
+    }
+
+    public void BuildStoryArray(TextAsset textFile)
+    {
+        storyLines = textFile.text.Split('\n');
+        Debug.Log(storyLines.Length);
     }
 }
