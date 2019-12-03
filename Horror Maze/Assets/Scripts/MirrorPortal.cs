a4bc090486979b8373429526ba63110c24d10fbb
jjlebow@gmail.com <jlebow@uci.edu>
2019-12-02 23:12:38 -0800

    added the intro scene

---
+++ Horror Maze/Assets/Scripts/MirrorPortal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MirrorPortal : MonoBehaviour
+{
+    public GameObject GM;
+    public Generator GMScript;
+    public FadeTransition fade;
+
+    private void OnTriggerEnter(Collider col)
+    {
+
+        if (col.GetComponent<Collider>().CompareTag("Player"))
+        {
+            Debug.Log("triggered");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+}
